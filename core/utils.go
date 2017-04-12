416ea90a18009adba6c65356682af7fb3c8c99dd
Daniel Fernandes Martins <daniel.martins@descomplica.com.br>
2017-04-12 17:36:35 -0300

    Add test for ParseCommaSeparatedList

--- core/utils.go
+++ core/utils.go
@@ -4,6 +4,8 @@ import (
 	"strings"
 )
 
+// ParseCommaSeparatedList takes a comma-separated string, such as "str1, str2",
+// and returns a list of pointers to each element.
 func ParseCommaSeparatedList(commaSeparatedList string) []*string {
 	items := []*string{}
 	rawList := strings.Split(commaSeparatedList, ",")
---
+++ core/utils_test.go
@@ -0,0 +1,62 @@
+package core
+
+import (
+	"testing"
+)
+
+func TestParseCommaSeparatedList(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			// No items
+			input:    "",
+			expected: []string{},
+		},
+		{
+			// No items
+			input:    "  ",
+			expected: []string{},
+		},
+		{
+			// No items
+			input:    " ,  ",
+			expected: []string{},
+		},
+		{
+			// One item
+			input:    "item-1",
+			expected: []string{"item-1"},
+		},
+		{
+			// One item
+			input:    " item-1, ",
+			expected: []string{"item-1"},
+		},
+		{
+			// One item
+			input:    " , item-1 , ",
+			expected: []string{"item-1"},
+		},
+		{
+			// Two items
+			input:    "item-1, item-2",
+			expected: []string{"item-1", "item-2"},
+		},
+	}
+
+	for _, testCase := range testCases {
+		output := ParseCommaSeparatedList(testCase.input)
+
+		if len(output) != len(testCase.expected) {
+			t.Errorf("Number of expected elements expected to be %d, but was %d", len(testCase.expected), len(output))
+		}
+
+		for i := range output {
+			if *output[i] != testCase.expected[i] {
+				t.Errorf("Expected output[%d] to be '%s', but was '%s'", i, testCase.expected[i], *output[i])
+			}
+		}
+	}
+}
